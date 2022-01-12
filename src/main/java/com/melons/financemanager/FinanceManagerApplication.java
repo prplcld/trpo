d879409237454c71bbd2e11df98285aa5fd10b5d
prplcld <antonsilebin@mail.ru>
2022-01-12 16:33:31 +0300

    tomcat fix

--- src/main/java/com/melons/financemanager/FinanceManagerApplication.java
+++ src/main/java/com/melons/financemanager/FinanceManagerApplication.java
@@ -3,6 +3,7 @@ package com.melons.financemanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,7 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EntityScan("com.melons.financemanager")
 @EnableJpaRepositories(basePackages = "com.melons.financemanager")
 @SpringBootApplication
-public class FinanceManagerApplication {
+public class FinanceManagerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceManagerApplication.class, args);
